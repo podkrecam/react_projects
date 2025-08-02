a9255ff281cda7625ab4d34a10777c53632d63bc
podkrecam <podkrecam@gmail.com>
2025-08-03 01:59:57 +0200

    dodanie sekcji contact oraz nodemailer

--- src/api/sendEmail.js
+++ src/api/sendEmail.js
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+  console.log(req);
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
