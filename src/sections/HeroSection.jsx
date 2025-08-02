307078c6ca77d0f1f923445e8400ab42c4a86ecc
podkrecam <podkrecam@gmail.com>
2025-08-03 00:23:38 +0200

    zmiana sekcji hero

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -12,10 +12,10 @@ export default function HeroSection() {
 
   return (
     <Section className="bg-primary text-text relative flex h-screen items-center justify-center overflow-hidden">
-      <div className="grid h-full w-full max-w-7xl grid-cols-1 items-center md:grid-cols-2">
+      <div className="grid h-full w-full max-w-7xl grid-cols-1 items-center justify-center gap-6 px-5 py-10 md:grid-cols-2">
         {/* lewa kolumna */}
         <div className="flex flex-col items-start justify-center space-y-6 px-5">
-          <h1 className="text-accent font-primary text-3xl font-bold tracking-tight uppercase md:text-8xl lg:text-9xl">
+          <h1 className="text-accent font-primary text-6xl font-bold tracking-tight uppercase md:text-8xl lg:text-9xl">
             Jadwiga Osial
           </h1>
           <p
@@ -23,17 +23,18 @@ export default function HeroSection() {
               animate ? "translate-y-0 opacity-100" : "translate-y-5 opacity-0"
             }`}
           >
-            Zanurz się w świecie mojej sztuki i pozwól, aby moje obrazy
-            inspirowały i poruszały Twoje zmysły.
+            Odkryj piękno pejzaży, portretów i abstrakcji pełnych emocji i
+            intensywnych kolorów. Zanurz się w świecie mojej sztuki i pozwól,
+            aby moje obrazy inspirowały i poruszały Twoje zmysły.
           </p>
         </div>
 
         {/* Prawa kolumna */}
-        <div className="flex max-h-4/5 justify-center px-2 md:px-16 xl:px-24">
+        <div className="flex max-h-full justify-center px-2 md:max-h-4/5 md:px-16 xl:px-24">
           <img
             src={hero1}
             alt="Obraz 1"
-            className={`max-w-full rounded-xl border border-neutral-800 object-cover shadow-2xl transition-transform duration-500 hover:scale-105 ${
+            className={`max-w-full rounded-xl border border-neutral-800 object-contain shadow-2xl transition-transform duration-500 hover:scale-105 md:object-cover ${
               animate ? "scale-100 opacity-100" : "scale-90 opacity-0"
             }`}
           />
