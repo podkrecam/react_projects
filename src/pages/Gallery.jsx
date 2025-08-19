52006296645ef0b7c9b2b7994aad89e03636e195
podkrecam <podkrecam@gmail.com>
2025-08-19 16:09:18 +0200

    dodanie dostepu do google drive w Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,25 +3,22 @@ import Section from "../components/Section";
 
 export default function Gallery() {
   const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     async function fetchImages() {
       const res = await fetch(
-        `https://www.googleapis.com/drive/v3/files?q='${import.meta.env.VITE_DRIVE_FOLDER_ID}'+in+parents&key=${import.meta.env.VITE_DRIVE_API}&fields=files(id,name,mimeType,webViewLink,webContentLink,thumbnailLink)`,
+        `https://www.googleapis.com/drive/v3/files?q='${import.meta.env.VITE_DRIVE_FOLDER_ID}'+in+parents&key=${import.meta.env.VITE_DRIVE_API}&fields=files(id,name,mimeType,thumbnailLink,webContentLink)`,
       );
-
       const data = await res.json();
-      console.log(data);
-
       const files = data.files
         .filter((file) => file.mimeType.startsWith("image/"))
         .map((file) => ({
           id: file.id,
           name: file.name,
-          thumbnail: file.thumbnailLink,
-          url: `https://drive.google.com/uc?export=view&id=${file.id}`,
+          thumbnail: file.thumbnailLink.replace("=s220", "=s640"),
+          full: file.thumbnailLink.replace("=s220", "=s1200"),
         }));
-
       setImages(files);
     }
     fetchImages();
@@ -29,17 +26,36 @@ export default function Gallery() {
 
   return (
     <Section className="bg-primary text-text min-h-screen p-8 pt-20">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {/* Masonry layout */}
+      <div className="columns-1 gap-4 space-y-4 sm:columns-2 md:columns-3 lg:columns-4">
         {images.map((file) => (
-          <div key={file.id} className="flex flex-col items-center">
+          <div
+            key={file.id}
+            className="cursor-pointer break-inside-avoid overflow-hidden rounded-lg shadow-lg"
+            onClick={() => setSelectedImage(file.full)}
+          >
             <img
-              src={file.url}
+              src={file.thumbnail}
               alt={file.name.split(".")[0]}
-              className="w-full rounded-lg object-cover shadow-lg"
+              className="h-auto w-full object-cover transition-transform duration-300 hover:scale-105"
             />
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      {selectedImage && (
+        <div
+          className="bg-opacity-80 fixed inset-0 z-100 flex items-center justify-center bg-black p-4"
+          onClick={() => setSelectedImage(null)}
+        >
+          <img
+            src={selectedImage}
+            alt="full view"
+            className="max-h-full max-w-full rounded-lg shadow-xl"
+          />
+        </div>
+      )}
     </Section>
   );
 }
