78b4c082202c6feb9567590f2178fd84bded6c08
podkrecam <podkrecam@gmail.com>
2025-08-19 15:28:30 +0200

    dodanie dostepu do google drive w Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,19 +1,13 @@
 import Section from "../components/Section";
 import { useEffect, useState } from "react";
 
-const a =
-  "352977332252-g2h1no8vvrlf2opusc1fbdnta959a7ce.apps.googleusercontent.com";
-
-const folderId = "1nmV92eolAFxjprEyWYt82i5BRl0srAVH";
-const apiKey = "AIzaSyAB9ldjOMSMVWsw_Q9SqRAjOWAdTLpdhCE";
-
 export default function Gallery() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     async function fetchImages() {
       const res = await fetch(
-        `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`,
+        `https://www.googleapis.com/drive/v3/files?q='${import.meta.env.DRIVE_FOLDER_ID}'+in+parents&key=${import.meta.env.DRIVE_API}&fields=files(id,name,mimeType)`,
       );
       const data = await res.json();
       const files = data.files
@@ -30,12 +24,14 @@ export default function Gallery() {
 
   return (
     <Section className="bg-primary text-text relative flex h-screen flex-col items-center justify-center overflow-hidden">
-      <h2 className="text-accent font-primary mb-10 text-4xl font-bold uppercase">
-        Strona w budowie...
-      </h2>
-      {images.map((img) => (
-        <div key={img.id} className="rounded shadow">
-          <img src={img.url} alt={img.name} className="h-auto w-full" />
+      {images.map((file) => (
+        <div key={file.id}>
+          <img
+            src={`https://drive.google.com/uc?export=view&id=${file.id}`}
+            alt={file.name}
+            className="rounded-lg shadow"
+          />
+          <p>{file.name}</p>
         </div>
       ))}
     </Section>
